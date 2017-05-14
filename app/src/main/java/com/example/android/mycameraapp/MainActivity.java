90a833a57c69a7bd8533d6f3c4557100c03cae29
WhitewolfDev <scottmartens43@gmail.com>
2017-05-14 14:57:01 -0700

    adding code to show bitmap of camera picture

--- app/src/main/java/com/example/android/mycameraapp/MainActivity.java
+++ app/src/main/java/com/example/android/mycameraapp/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.android.mycameraapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    Bitmap imageBitmap;
+    ImageView myCameraImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // display camera capture image
+        myCameraImage = (ImageView) findViewById(R.id.showAPictureIcon);
+
+        // take a picture
         Button pictureButton = (Button) findViewById(R.id.takeAPictureButton);
         View.OnClickListener pictureListener = new View.OnClickListener() {
             @Override
@@ -33,6 +42,15 @@ public class MainActivity extends AppCompatActivity {
         // run the startActivity that no app can handle will crash the program.
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extra = data.getExtras();
+            imageBitmap = (Bitmap) extra.get("data");
+            myCameraImage.setImageBitmap(imageBitmap);
         }
     }
 }
