7a5dfbce62953f47c00d5b106bab311ce365560b
WhitewolfDev <scottmartens43@gmail.com>
2017-05-14 13:59:14 -0700

    added the code to call activity to take the picture

--- app/src/main/java/com/example/android/mycameraapp/MainActivity.java
+++ app/src/main/java/com/example/android/mycameraapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.android.mycameraapp;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button pictureButton = (Button) findViewById(R.id.takeAPictureButton);
+        View.OnClickListener pictureListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // taking the picture
+                dispatchTakePictureIntent();
+            }
+        };
+        pictureButton.setOnClickListener(pictureListener);
+    }
+
+    private void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
