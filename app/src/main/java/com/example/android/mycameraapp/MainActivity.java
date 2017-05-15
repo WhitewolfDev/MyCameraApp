623c8d094e5e3a66b21cb66ff9c71b69c76c029c
WhitewolfDev <scottmartens43@gmail.com>
2017-05-14 23:07:11 -0700

    code will save on device folder, but not sdcard folder

--- app/src/main/java/com/example/android/mycameraapp/MainActivity.java
+++ app/src/main/java/com/example/android/mycameraapp/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.android.mycameraapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
-    Bitmap imageBitmap;
-    ImageView myCameraImage;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int IMAGE_REQUEST_CODE = 1;
+
+    private Bitmap imageBitmap;
+    private ImageView myCameraImage;
+    private String mCurrentPhotoPath;
+    private Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,16 +56,50 @@ public class MainActivity extends AppCompatActivity {
         // if nothing can handle the intent, a null is returned.  Otherwise, trying to
         // run the startActivity that no app can handle will crash the program.
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                // ...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extra = data.getExtras();
-            imageBitmap = (Bitmap) extra.get("data");
+            //Bundle extra = data.getExtras();
+            //imageBitmap = (Bitmap) extra.get("data");
+            imageBitmap = BitmapFactory.decodeFile(String.valueOf(photoURI));
             myCameraImage.setImageBitmap(imageBitmap);
         }
+    }
+
+    // The main problem with this is that it did not store images to the sdcard, but the local copy
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir /* directory */
+                );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
